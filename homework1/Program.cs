b20629c0c1bb96a93293802a62f20572cf9ceeef
gulzara2022 <gulzaraga@code.edu.az>
2022-03-18 23:35:39 +0400

    v1

---
+++ homework1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            // CTRL + K + C
+            //int a = 98;
+
+            //int b = a % 3;
+
+            //if (b == 0)
+            //{
+            //    console.writeline("yes");
+            //}
+            //else if (b != 0)
+            //{
+            //    console.writeline("no");
+            //}
+            #endregion
+
+            #region Task2
+            //int a = 22;
+
+            //if(a % 2 == 0 && a % 3 == 0)
+            //{
+            //    Console.WriteLine("3 e bolunur cutdur");
+            //}
+            //else if (a % 3 == 0)
+            //{
+            //    Console.WriteLine("3 e bolunur tekdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("3 e bolunmur");
+            //}
+            #endregion
+
+            #region Task 3
+            //int a = 1000;
+            //int b = 10000;
+            //int c = a + b;
+
+            //if (c >= 0 && c < 10)
+            //{
+            //    Console.WriteLine("1 reqemli");
+            //}
+            //else if (c >= 10 && c < 100)
+            //{
+            //    Console.WriteLine("2 reqemli");
+            //}
+            //else if (c >= 100 && c < 1000)
+            //{
+            //    Console.WriteLine("3 reqemli");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("3 reqemliden artiq nezere alinmiyib");
+            //}
+
+
+            #endregion
+
+            #region Task 4
+
+
+            //int c = 100;
+
+            //if(c >= 0 && c <10)
+            //{
+            //    Console.WriteLine("1 reqemli");
+            //}
+            //else if(c >= 10 && c < 100)
+            //{
+            //    Console.WriteLine("2 reqemli");
+            //}
+            //else if (c >= 100 && c < 1000)
+            //{
+            //    Console.WriteLine("3 reqemli");
+            //}
+            //else if (c >= 1000 && c < 10000)
+            //{
+            //    Console.WriteLine("4 reqemli");
+            //}
+            //else if (c >= 10000 && c < 100000)
+            //{
+            //    Console.WriteLine("5 reqemli");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("5 reqemliden artiq nezere alinmiyib");
+            //}
+
+            #endregion
+
+            #region Task 5
+
+            //int a = 11;
+            //int b = 2;
+
+
+            //if(a % b == 0)
+            //{
+            //    Console.WriteLine("Tam Bolunur");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Bolunmur");
+            //}
+
+            #endregion
+        }
+    }
+}
